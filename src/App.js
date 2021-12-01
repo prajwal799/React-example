99ce6df3bdfb7086be9cb0cdcd520b046922d59d
Prajwal goyal <75352182+prajwal799@users.noreply.github.com>
2021-12-01 14:22:32 +0530

    Count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./styles.css";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const ref = useRef(null);
+  useEffect(() => {
+    startTime();
+  }, []);
+
+  const startTime = () => {
+    ref.current = setInterval(() => {
+      setCount((prev) => {
+        return prev + 1;
+      });
+    }, 1000);
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h1>Count</h1>
+      <h1>{count}</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
